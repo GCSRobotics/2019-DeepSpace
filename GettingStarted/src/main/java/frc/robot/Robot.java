3e0e203f59030c59ac2ef75f647e4148397d01e5
Zane2112 <xanadu1806@gmail.com>
2019-01-17 18:35:35 -0500

    changes

--- GettingStarted/src/main/java/frc/robot/Robot.java
+++ GettingStarted/src/main/java/frc/robot/Robot.java
@@ -7,10 +7,7 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
@@ -21,8 +18,10 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * directory.
  */
 public class Robot extends TimedRobot {
+  private final SpeedControllerGroup speedControllerGroupleft = new SpeedControllerGroup(new PWMVictorSPX(0), new PWMVictorSPX(1));
+  private final SpeedControllerGroup speedControllerGroupright = new SpeedControllerGroup(new PWMVictorSPX(2), new PWMVictorSPX(3));
   private final DifferentialDrive m_robotDrive
-      = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+      = new DifferentialDrive(speedControllerGroupleft, speedControllerGroupright);
   private final Joystick m_stick = new Joystick(0);
   private final Timer m_timer = new Timer();
 
@@ -30,14 +29,9 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
-  // This is  comment
+  // This is  comment. no its not
   @Override
   public void robotInit() {
-  }
-
-  public int GetInt()
-  {
-    return 2;
   }
 
   /**
